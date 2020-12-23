948790f680d4705fa6b70562c943d27640f70388
PriyankaScalent <priyanka@scalent.io>
2020-12-23 19:24:48 +0530

    basic rest api implemetation

--- databases/create_users_mysql.go
+++ databases/create_users_mysql.go
@@ -1,8 +1,43 @@
 package databases
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	ID        uint   `gorm:"not null"`
+	Fname     string `gorm:"not null"`
+	Lname     string
+	Email     *string
+	DOB       *time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
 func CreateUsersTable() {
-	ConnectDB()
-	fmt.Println("inside cu >>>>>>>>>>>>>>>")
+	//ConnectDB()
+
+	sqlDB.LogMode(true)
+
+	sqlDB, err := gorm.Open("mysql", "root:password@tcp(127.0.0.1:3306)/test")
+
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("connected to db successfuly")
+
+	// Create table for `User`
+	sqlDB.AutoMigrate().DropTable(&User{})
+
+	// Create table for `User`
+	sqlDB.AutoMigrate().CreateTable(&User{})
+
+	// Append "ENGINE=InnoDB" to the creating table SQL for `User`
+	sqlDB.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate().CreateTable(&User{})
+
 }
--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"assignments/databases"
+	//"assignments/databases"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
+type User struct {
+	ID    int    `json:"ID"`
+	Fname string `json:"Fname"`
+	Lname string `json:"Lname"`
+	Email string `json:"Email"`
+	DOB   string `json:"DOB"`
+}
+
+var Users []User
+
+func returnAllUsers(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllUsers")
+	json.NewEncoder(w).Encode(Users)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/users", returnAllUsers)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
 func main() {
-	databases.CreateUsersTable()
+	//databases.CreateUsersTable()
+
+	Users = []User{
+		User{ID: 1, Fname: "John", Lname: "Ray", Email: "john@gmail.com", DOB: "19-04-1998"},
+		User{ID: 2, Fname: "Will", Lname: "Smith", Email: "will@gmail.com", DOB: "12-03-1990"},
+	}
+
+	handleRequests()
 }
